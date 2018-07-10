6ace9997c7c48c9ffd56559540164b145c0e925e
smilezhaop <805673650@qq.com>
2018-07-10 14:19:19 +0800

    seven-eigth

---
+++ src/main/java/com/qingguatang/spider/service/DownloadService.java
@@ -0,0 +1,9 @@
+package com.qingguatang.spider.service;
+
+/**
+ * @author zhaopei
+ * @date 2018/7/6
+ */
+public interface DownloadService {
+  public void download(String fileName,String url);
+}
